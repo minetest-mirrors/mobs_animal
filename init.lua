d4ae151e47237189e295d2495c885a7a9d3d8251
tenplus1 <kinsellaja@yahoo.com>
2020-08-25 09:56:14 +0100

    update translation detection

--- init.lua
+++ init.lua
@@ -1,13 +1,11 @@
 
-local path = minetest.get_modpath("mobs_animal")
-
 -- Load support for intllib.
-local MP = minetest.get_modpath(minetest.get_current_modname())
-local S, NS = dofile(MP.."/intllib.lua")
+local path = minetest.get_modpath(minetest.get_current_modname())
+local S = minetest.get_translator and minetest.get_translator("mobs_animal") or
+		dofile(path .. "/intllib.lua")
 mobs.intllib = S
 
 -- Animals
-
 dofile(path .. "/chicken.lua") -- JKmurray
 dofile(path .. "/cow.lua") -- KrupnoPavel
 dofile(path .. "/rat.lua") -- PilzAdam
@@ -19,6 +17,7 @@ dofile(path .. "/kitten.lua") -- Jordach/BFD
 dofile(path .. "/penguin.lua") -- D00Med
 dofile(path .. "/panda.lua") -- AspireMint
 
+-- Lucky Blocks
 dofile(path .. "/lucky_block.lua")
 
 print (S("[MOD] Mobs Redo Animals loaded"))
